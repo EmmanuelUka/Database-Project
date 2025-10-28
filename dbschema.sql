3893bfada874960df95b3faf2fa4200af0ef5025
emmanueluc322 <61635797+emmanueluc322@users.noreply.github.com>
2025-10-28 13:34:10 -0400

    Revert "done with tables"
    This reverts commit c879f99f9a09479c3702fd410810ceea541b6905.

--- dbschema.sql
+++ dbschema.sql
@@ -2,44 +2,6 @@ create schema euka; --change schema username to yours
 use euka;
 
 create table student{
-    student_ID          varchar(10),
-    s_name              varchar(25),
-    dept_ID             varchar(10),
-    tot_credits         numeric(2,0),
-    GPA                 numeric(1,2),
-    email               varchar(30),
-    address_houseNumber varchar(4),
-    address_street      varchar(25),
-    address_city,       varchar(25),
-    address_state       varchar(15),
-    address_zip         numeric(5,0),
+    student_ID varchar(10)
+    --
 }
-
-create table professor{
-    professor_ID        varchar(10),
-    p_name              varchar(25),
-    dept_ID             varchar(10),
-    salary              numeric(6,2),
-    email               varchar(30),
-    address_houseNumber varchar(4),
-    address_street      varchar(25),
-    address_city,       varchar(25),
-    address_state       varchar(15),
-    address_zip         numeric(5,0),
-}
-
-create table section{
-    section_ID              varchar(10),
-    course_ID               varchar(10),
-    professor_ID            varchar(10),
-    semester                varchar(6)
-		check (semester in ('Fall', 'Winter', 'Spring', 'Summer')), 
-    year			numeric(4,0) check (year > 1701 and year < 2100), 
-    section_number          varchar(3),
-    room_number    varchar(3),
-    days           varchar(2)
-		check (semester in ('M', 'T', 'W', 'Th', 'F')),
-    time          varchar(5),
-    capacity varchar(3),
-}
-
