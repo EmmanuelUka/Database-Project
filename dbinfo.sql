54df084f032db71bf10c6613b3e657b66048f5c3
Zac-Evans62 <pvr55w2d67@privaterelay.appleid.com>
2025-10-29 15:47:49 -0400

    fixed varchar sizes

--- dbinfo.sql
+++ dbinfo.sql
@@ -1,1 +1,16 @@
 delete from classroom;
+
+insert into professor
+values('1273892037', 'James Evans', '1001', 100000, 'j.evans@kent.edu', '120', 'county road 12', 'toledo', 'ohio', 23495 );
+
+INSERT INTO professor
+VALUES ('2384920183', 'Sarah Thompson', '1002', 87500, 's.thompson@kent.edu', '45', 'Maple Street', 'Cleveland', 'Ohio', 44114);
+
+INSERT INTO professor
+VALUES ('3471829401', 'Robert Jenkins', '1003', 94500, 'r.jenkins@kent.edu', '210', 'Lakeview Drive', 'Akron', 'Ohio', 44308);
+
+INSERT INTO professor
+VALUES ('4592038174', 'Emily Rivera', '1004', 91000, 'e.rivera@kent.edu', '18', 'Sunset Blvd', 'Columbus', 'Ohio', 43215);
+
+INSERT INTO professor
+VALUES ('5610283947', 'Michael Chen', '1005', 99000, 'm.chen@kent.edu', '300', 'Summit Avenue', 'Kent', 'Ohio', 44240);
--- dbschema.sql
+++ dbschema.sql
@@ -3,7 +3,7 @@ use euka;
 
 create table student(    student_ID          varchar(10) primary key,
     s_name              varchar(25) not null,
-    dept_ID             varchar(10),
+    dept_ID             varchar(4),
     tot_credits         numeric(2,0),
     GPA                 numeric(1,2),
     email               varchar(30),
@@ -18,7 +18,7 @@ create table student(    student_ID          varchar(10) primary key,
 create table professor(
     proffesor_ID        varchar(10) primary key,
     p_name              varchar(25) not null,
-    dept_ID             varchar(10),
+    dept_ID             varchar(4),
     salary              numeric(6,2),
     email               varchar(30),
     address_houseNumber varchar(4),
@@ -51,7 +51,7 @@ create table course(
     course_ID varchar(10) primary key,
     c_name varchar(25) not null,
     credits numeric(1, 0),
-    department_ID varchar(10),
+    department_ID varchar(4),
     foreign key (department_ID) references department(department_ID)
 );
 
@@ -64,7 +64,7 @@ create table clasroom(
 );
 
 create table building (
-    department_ID varchar(10),
+    department_ID varchar(4),
     b_name varchar(25) primary key,
     floor_count numeric(2,0),
     classroom_count numeric(3,0),
@@ -79,7 +79,7 @@ create table department(
     d_name varchar(25) not null,
     building_ID varchar(10),
     budget numeric(9,2),
-    department_ID varchar(10) primary key,
+    department_ID varchar(4) primary key,
 );
 
 create table takes(
