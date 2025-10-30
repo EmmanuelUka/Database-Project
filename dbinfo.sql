64b154fad3ac9874b98cc172e369b416bef75f9b
emmanueluc322 <61635797+emmanueluc322@users.noreply.github.com>
2025-10-30 11:07:27 -0400

    added classromm and removed queries

--- dbinfo.sql
+++ dbinfo.sql
@@ -20,21 +20,6 @@ insert into professor
 values ('5610283947', 'Michael Chen', '1005', 99000, 'm.chen@kent.edu', '300', 'Summit Avenue', 'Kent', 'Ohio', 44240);
 
 
-
-select * from professor;
-
-
-select * from professor where ID ='';
-
-
-update professor
-set salary = ''
-where professor_ID = '';
-
-
-delete from professor where professor_ID = '';
-
-
 insert into student 
 values ('9515617924', 'Emmanuel Uka', '12', 3.95, 'emauka@gmail.com', '2000', 'Monster', 'Hell', 'Ohio', '12446');
 
@@ -51,39 +36,11 @@ insert into student
 values ('9515617928', 'noah patel', '9', 3.60, 'npatel@gmail.com', '678', 'sunset drive', 'willowtown', 'ny', '10027');
 
 
-select * from students
-
-
-select * from professor where student_ID = '3471829401';
-
-
-update student
-set GPA = '4.00'
-where student_ID = '9515617924';
-
-
-delete from student where student_ID = '9515617926';
-
-
 insert into course values('CS101', 'Introduction to Computer Science', '4', '1001');
 insert into course values('MATH201', 'Calculus I', '4', '1002');
 insert into course values('ENG150', 'English Literature', '3', '1003');
 insert into course values('HIST210', 'World History', '3', '1004');
 insert into course values('BIO110', 'General Biology', '4', '1005');
-
-
-select * from courses;
-
-
-select * from course where course_ID = 'HIST210';
-
-
-update course
-set credits = '10'
-where course_ID = 'CS101';
-
-
-delete from course where course_ID = 'HIST210';
 
 
 insert into teaches values('1273892037', '001');
@@ -99,21 +56,6 @@ insert into section values('ENG150','3471829401','Fall', '2023','003', '404', 'F
 insert into section values('HIST210','4592038174','Spring', '2024','004', '303', 'Th', '2:15', '110');
 insert into section values('BIO110','5610283947','Fall', '2023','005', '505', 'W', '12:30', '45');
 
-
-select * from section;
-
-
-select * from section where course_ID = 'ENG150';
-
-
-update section
-set time = '4:00'
-where course_ID = 'ENG150' & section_ID = '003';
-
-
-delete from section where course_ID = 'BIO110' & section_ID = '005';
-
-
 insert into department
 values('computer science', 'conner hall', 500000, 1001);
 
@@ -121,28 +63,13 @@ insert into department
 VALUES ('Mathematics', 'Taylor Hall', 420000, 1002);
 
 insert into department
-VALUES ('Physics', 'Franklin Hall', 380000, 1003);
+VALUES ('History', 'Franklin Hall', 380000, 1003);
 
 insert into department
 VALUES ('English', 'Johnson Hall', 310000, 1004);
 
 insert into department
-VALUES ('Psychology', 'Anderson Hall', 460000, 1005);
-
-
-select * from department
-
-
-select * from department where department_ID = 'computer science'
-
-
-update department 
-set budget = 0
-where department_ID = 'English'
-
-
-delete from department where department_ID = 'Psychology';
-
+VALUES ('Life Sciences', 'Anderson Hall', 460000, 1005);
 
 insert into takes
  values ('9515617924', '001', 'A');
@@ -154,9 +81,6 @@ insert into takes
  values ('9515617927', '004', 'A');
 insert into takes
  values ('9515617928', '005', 'B');
-
-
-delete from takes where student_ID = '9515617925' & section_number = '002'
 
 
 insert into building 
@@ -175,15 +99,8 @@ insert into building
 values ('Anderson Hall', 4, 20, '460', 'Summit Street', 'Kent', 'Ohio', 44240);
 
 
-select * from building
-
-
-select * from building where department_ID = 'Psychology';
-
-
-update building
-set b_name = 'Olson Hall'
-where b_name = 'Anderson Hall'
-
-
-delete from building where building_name = 'Johnson Hall';
+insert into classroom values ('Conner Hall', '101', 50);
+insert into classroom values ('Taylor Hall', '202', 30);
+insert into classroom values ('Franklin Hall', '303', 40);  
+insert into classroom values ('Johnson Hall', '404', 20);
+insert into classroom values ('Anderson Hall', '505', 35);
