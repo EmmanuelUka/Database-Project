f0953e2e41df4ecff072708709302b210f9813c3
emmanueluc322 <61635797+emmanueluc322@users.noreply.github.com>
2025-11-12 17:01:47 -0500

    test

--- app.py
+++ app.py
@@ -6,11 +6,32 @@ app = Flask(__name__)
 app.secret_key = "Cleveland_Browns"
 
 db_config = {
-    'user': 'zevans6', 
-    'password': 'k38ndcYQ',
+    'user': 'euka', 
+    'password': '@Dubem0610',
     'host': 'dbdev.cs.kent.edu',
-    'database': 'zevans6'
+    'database': 'euka'
 }
+
+"""@app.route('/')
+def home():
+    return render_template('index.html')"""
+    
+@app.route('/')
+def home():
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("SELECT student_name, student_id, gpa FROM student;")
+        students = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return render_template('index.html', students=students)
+    except Exception as e:
+        return f"Database connection failed: {e}"
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+
+    
+ 
