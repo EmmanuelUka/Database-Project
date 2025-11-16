1419613ec45992d304935c87dbbab71f3ce0acec
emmanueluc322 <61635797+emmanueluc322@users.noreply.github.com>
2025-11-16 15:26:09 -0500

    Revert "corrected db"
    This reverts commit 3187b3cf4e772f97f4d8268c5a3478ba58f84659.

--- dbschema.sql
+++ dbschema.sql
@@ -1,111 +1,113 @@
-USE euka;
+use project;
 
-CREATE TABLE building (
-    b_name VARCHAR(25) PRIMARY KEY,
-    floor_count INT,
-    classroom_count INT,
-    address_building_number VARCHAR(4),
-    address_street VARCHAR(25),
-    address_city VARCHAR(25),
-    address_state VARCHAR(25),
-    address_zip INT
+
+create table building (
+    b_name varchar(25) primary key,
+    floor_count numeric(2,0),
+    classroom_count numeric(3,0),
+    address_building_number varchar(4),
+    address_street varchar(25),
+    address_city varchar(25),
+    address_state varchar(25),
+    address_zip numeric(5,0)
 );
 
-CREATE TABLE classroom (
-    b_name VARCHAR(25),
-    room_number VARCHAR(3),
-    capacity INT,
-    PRIMARY KEY (b_name, room_number),
-    FOREIGN KEY (b_name) REFERENCES building(b_name)
+create table classroom (
+    b_name varchar(25),
+    room_number varchar(3),
+    capacity varchar(3),
+    primary key (b_name, room_number),
+    foreign key (b_name) references building(b_name)
 );
 
-CREATE TABLE department (
-    department_id VARCHAR(4) PRIMARY KEY,
-    d_name VARCHAR(25) NOT NULL,
-    b_name VARCHAR(25),
-    budget DECIMAL(9,2),
-    FOREIGN KEY (b_name) REFERENCES building(b_name)
+create table department (
+    department_id varchar(4) primary key,
+    d_name varchar(25) not null,
+    b_name varchar(25),
+    budget numeric(9,2),
+    foreign key (b_name) references building(b_name)
 );
 
-CREATE TABLE course (
-    course_id VARCHAR(10) PRIMARY KEY,
-    c_name VARCHAR(25) NOT NULL,
-    credits INT,
-    department_id VARCHAR(4),
-    FOREIGN KEY (department_id) REFERENCES department(department_id)
+create table course (
+    course_id varchar(10) primary key,
+    c_name varchar(25) not null,
+    credits numeric(1,0),
+    department_id varchar(4),
+    foreign key (department_id) references department(department_id)
 );
 
-CREATE TABLE users (
-    user_id VARCHAR(10) PRIMARY KEY,
-    username VARCHAR(25) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role ENUM('admin', 'instructor', 'student') NOT NULL
+create table users (
+    user_id varchar(10) primary key,
+    username varchar(25) unique not null,
+    password varchar(255) not null,
+    role enum('admin', 'instructor', 'student') not null
 );
 
-CREATE TABLE professor (
-    professor_id VARCHAR(10) PRIMARY KEY,
-    p_name VARCHAR(25) NOT NULL,
-    dept_id VARCHAR(4),
-    salary DECIMAL(8,2),
-    email VARCHAR(30),
-    address_houseNumber VARCHAR(4),
-    address_street VARCHAR(25),
-    address_city VARCHAR(25),
-    address_state VARCHAR(15),
-    address_zip INT,
-    FOREIGN KEY (dept_id) REFERENCES department(department_id),
-    FOREIGN KEY (professor_id) REFERENCES users(user_id)
+create table professor (
+    professor_id varchar(10) primary key,
+    p_name varchar(25) not null,
+    dept_id varchar(4),
+    salary numeric(6,2),
+    email varchar(30),
+    address_houseNumber varchar(4),
+    address_street varchar(25),
+    address_city varchar(25),
+    address_state varchar(15),
+    address_zip numeric(5,0),
+    foreign key (dept_id) references department(department_id),
+    foreign key (professor_id) references users(user_id)
 );
 
-CREATE TABLE student (
-    student_id VARCHAR(10) PRIMARY KEY,
-    s_name VARCHAR(25) NOT NULL,
-    dept_id VARCHAR(4),
-    tot_credits INT,
-    gpa DECIMAL(3,2),
-    email VARCHAR(30),
-    address_houseNumber VARCHAR(4),
-    address_street VARCHAR(25),
-    address_city VARCHAR(25),
-    address_state VARCHAR(15),
-    address_zip INT,
-    FOREIGN KEY (dept_id) REFERENCES department(department_id),
-    FOREIGN KEY (student_id) REFERENCES users(user_id)
+create table student (
+    student_id varchar(10) primary key,
+    s_name varchar(25) not null,
+    dept_id varchar(4),
+    tot_credits numeric(2,0),
+    gpa decimal(3,2),
+    email varchar(30),
+    address_houseNumber varchar(4),
+    address_street varchar(25),
+    address_city varchar(25),
+    address_state varchar(15),
+    address_zip numeric(5,0),
+    foreign key (dept_id) references department(department_id),
+    foreign key (student_id) references users(user_id)
 );
 
-CREATE TABLE section (
-    section_number VARCHAR(10) PRIMARY KEY,
-    course_id VARCHAR(10),
-    professor_id VARCHAR(10),
-    semester VARCHAR(6) CHECK (semester IN ('Fall','Winter','Spring','Summer')),
-    year INT CHECK (year > 1701 AND year < 2100),
-    room_number VARCHAR(3),
-    b_name VARCHAR(25),
-    days VARCHAR(2) CHECK (days IN ('M','T','W','Th','F')),
-    time VARCHAR(5),
-    capacity INT,
-    FOREIGN KEY (course_id) REFERENCES course(course_id),
-    FOREIGN KEY (professor_id) REFERENCES professor(professor_id),
-    FOREIGN KEY (b_name, room_number) REFERENCES classroom(b_name, room_number)
+create table section (
+    section_number varchar(10) primary key,
+    course_id varchar(10),
+    professor_id varchar(10),
+    semester varchar(6) check (semester in ('Fall', 'Winter', 'Spring', 'Summer')),
+    year numeric(4,0) check (year > 1701 and year < 2100),
+    room_number varchar(3),
+    b_name varchar(25),
+    days varchar(2) check (days in ('M', 'T', 'W', 'Th', 'F')),
+    time varchar(5),
+    capacity varchar(3),
+    foreign key (course_id) references course(course_id),
+    foreign key (professor_id) references professor(professor_id),
+    foreign key (b_name, room_number) references classroom(b_name, room_number)
 );
 
-CREATE TABLE takes (
-    student_id VARCHAR(10),
-    section_number VARCHAR(10),
-    letter VARCHAR(1) CHECK (letter IN ('A','B','C','D','F')),
-    course_id VARCHAR(10),
-    PRIMARY KEY (student_id, section_number),
-    FOREIGN KEY (student_id) REFERENCES student(student_id),
-    FOREIGN KEY (section_number) REFERENCES section(section_number),
-    FOREIGN KEY (course_id) REFERENCES course(course_id)
+create table takes (
+    student_id varchar(10),
+    section_number varchar(3),
+    letter varchar(1) check (letter in ('A', 'B', 'C', 'D', 'F')),
+    course_ID varchar(10),
+    primary key (student_id, section_number),
+    foreign key (student_id) references student(student_id),
+    foreign key (section_number) references section(section_number),
+    foreign key (course_id) references course(course_id)
 );
 
-CREATE TABLE teaches (
-    professor_id VARCHAR(10),
-    section_number VARCHAR(10),
-    course_id VARCHAR(10),
-    PRIMARY KEY (professor_id, section_number),
-    FOREIGN KEY (professor_id) REFERENCES professor(professor_id),
-    FOREIGN KEY (section_number) REFERENCES section(section_number),
-    FOREIGN KEY (course_id) REFERENCES course(course_id)
+create table teaches (
+    professor_id varchar(10),
+    section_number varchar(3),
+    course_ID varchar(10),
+    primary key (professor_id, section_number),
+    foreign key (professor_id) references professor(professor_id),
+    foreign key (section_number) references section(section_number),
+    foreign key (course_id) references course(course_id)
 );
+
